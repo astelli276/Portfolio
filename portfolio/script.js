a689f18ec52ab4ab32f07509b7c7880b60413792
astelli276 <45165665+astelli276@users.noreply.github.com>
2018-12-03 13:57:57 +0100

    updated a bunch of stuff

---
+++ portfolio/script.js
@@ -0,0 +1,34 @@
+	function openNav() {
+		document.getElementById("sidenav").style.width = "250px";
+	}
+	function closeNav() {
+		document.getElementById("sidenav").style.width = "0";
+	}
+	function openProject(evt, cityName) {
+	    var i, tabcontent, tablinks;
+	    tabcontent = document.getElementsByClassName("tabcontent");
+	    for (i = 0; i < tabcontent.length; i++) {
+	        tabcontent[i].style.display = "none";
+	    }
+	    tablinks = document.getElementsByClassName("tablinks");
+	    for (i = 0; i < tablinks.length; i++) {
+	        tablinks[i].className = tablinks[i].className.replace(" active", "");
+	    }
+	    document.getElementById(cityName).style.display = "block";
+	    evt.currentTarget.className += " active";
+	}
+
+
+	window.onscroll = function(){myFunction()};
+
+	var header = document.getElementById("myHeader");
+	var sticky = header.offsetTop;
+
+	function myFunction() {
+		if (window.pageYOffset > sticky){
+			header.classList.add("sticky");
+		}
+		else {
+			header.classList.remove("sticky");
+		}
+	}
